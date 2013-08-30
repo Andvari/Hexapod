80e0bac66a8c586cff1ebf5bd5d04da2f0626f93
nemo <dedmazai74@mail.ru>
2013-08-30 07:32:05 +0400

    Next iteration

--- Servo.cpp
+++ Servo.cpp
@@ -6,13 +6,62 @@
  */
 
 #include "Servo.h"
+#include "stdring.h"
 
-Servo::Servo() {
-	// TODO Auto-generated constructor stub
+void Servo::Servo(int new_base, int new_bias, int *p[]){
+	this->base = new_base;
+	this_bias = new_bias;
 
+	for(int i=0; i<NUM_MOTIONS; i++){
+		for(int j=0; j<NUM_PHASES; j++){
+			this->phases[i][j] = p[i][j];
+		}
+	}
+
+	for(i=0; i<sizeof(this->command); i++){
+		this->command[i] = 0;
+	}
+
+	this->command[0] = "P";
 }
 
-Servo::~Servo() {
-	// TODO Auto-generated destructor stub
+int Servo :: startMotion(int new_motion){
+	if (this->isInMotion()) return FALSE;
+	if((new_motion < 0)||(new_motion > NUM_MOTIONS - 1)) return FALSE;
+	this->motion = new_motion;
+	this->phase = 0;
+	return TRUE;
 }
 
+int Servo :: nextPhase(void){
+	if (this->phase == NUM_PHASES - 1) return FALSE;
+	this->phase ++;
+	return TRUE;
+}
+
+int Servo :: isInMotion(void){
+	if (this->phase == NUM_PHASES-1){
+		return FALSE;
+	}
+	else{
+		return TRUE;
+	}
+}
+
+int Servo :: isStopped(void){
+	if (this->phase == NUM_PHASES-1){
+		return TRUE;
+	}
+	else{
+		return FALSE;
+	}
+}
+
+void reset(void){
+	this->motion = -1;
+	this->phase = -1;
+}
+
+char* getCommand(void){
+	return this->command;
+}
--- Servo.h
+++ Servo.h
@@ -9,9 +9,25 @@
 #define SERVO_H_
 
 class Servo {
+
+private:
+	int		base	=	-1;
+	int		bias	=	-1;
+	int		motion	=	-1;
+	int		phase	=	-1;
+	int		phases[NUM_MOTIONS][NUM_PHASES];
+	char	command[8];
+
 public:
-	Servo();
-	virtual ~Servo();
+	void	Servo(int, int, int *[]);
+	virtual	~Servo();
+
+	int		startMotion(int);
+	int		nextPhase(void);
+	int		isInMotion(void);
+	int		isStopped(void);
+	void	reset(void);
+	char*	getCommand(void);
 };
 
 #endif /* SERVO_H_ */
